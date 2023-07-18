5c14c2c23eb15e9341e0adc1c1503968a31198fc
ADV Indian coder <116486037+ADVindiancoder@users.noreply.github.com>
2023-07-17 23:26:36 -0700

    Binary-Search-LeetCode-ADV-

---
+++ Solution1.java
@@ -0,0 +1,31 @@
+class Solution1 {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int n1 = nums1.length;
+        int n2 = nums2.length;
+        if (n1 > n2) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
+        int k = (n1 + n2 + 1) / 2;
+        int left = 0;
+        int right = n1;
+        while (left < right) {
+            int mid1 = (left + right) / 2;
+            int mid2 = k - mid1;
+            if (nums1[mid1] < nums2[mid2 - 1]) {
+                left = mid1 + 1;
+            } else {
+                right = mid1;
+            }
+        }
+        int mid1 = left;
+        int mid2 = k - left;
+        int c1 = Math.max(mid1 <= 0 ? Integer.MIN_VALUE : nums1[mid1 - 1], 
+                          mid2 <= 0 ? Integer.MIN_VALUE : nums2[mid2 - 1]);
+        if ((n1 + n2) % 2 == 1) {
+            return c1;
+        }
+        int c2 = Math.min(mid1 >= n1 ? Integer.MAX_VALUE : nums1[mid1], 
+                          mid2 >= n2 ? Integer.MAX_VALUE : nums2[mid2]);
+        return (c1 + c2) * 0.5;
+    }
+}
---
+++ Solution2.java
@@ -0,0 +1,30 @@
+class Solution2 {
+    public int search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            
+            if (nums[mid] == target) {
+                return mid;
+            }
+            
+            // If left half is sorted
+            if (nums[left] <= nums[mid]) {
+                if (target >= nums[left] && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else { // If right half is sorted
+                if (target > nums[mid] && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+        
+        return -1;
+    }
+}
